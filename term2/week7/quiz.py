8a4b0a0a809a555cdebbbaa698ca6a6e63f81fb9
FusionSid <siddheshadsv@icloud.com>
2022-06-15 11:27:16 +1200

    Fixed quiz game

--- term2/week7/quiz.py
+++ term2/week7/quiz.py
@@ -1,5 +1,7 @@
 import os
+import time
 import json
+import random
 
 from rich.console import Console
 
@@ -7,32 +9,54 @@ console = Console()
 
 play = console.input("[blue bold]Would you like to play? (Yes/No) ").lower()
 if play not in ["yes", "y"]:
+    console.print("[purple bold]ok!\n")
     quit(0)
 
 points = 0
 num = 1
 
 with open("questions.json") as f:
-    questions_and_answers = json.load(f)
+    questions_and_answers_ps = json.load(f)
 
-console.print("[purple bold]Welcome to the quiz!\n")
-input("press any key to start...")
+    shuffled_qa = list(questions_and_answers_ps.items())
+
+    random.shuffle(shuffled_qa)
+
+    questions_and_answers = {}
+
+    for k, v in shuffled_qa:
+        questions_and_answers[k] = v
+
+
+console.print("[purple bold]Welcome to the quiz!")
+console.input("[red]press any key to start...")
 
 for question, answer in questions_and_answers.items():
     os.system("clear")
     console.print(f"[yellow]Q{num}: {question}?")
     user_ans = console.input("(true/false): ").lower()
-    user_ans = True if user_ans in ["yes", "true", "t", "y", "1"] else False
+
+    if user_ans in ["yes", "true", "t", "y", "1"]:
+        user_ans = True
+    elif user_ans in ["no", "false", "f", "n", "0"]:
+        user_ans = False
+    else:
+        user_ans = None
 
     num += 1
 
     if user_ans == answer:
         points += 1
         console.print("[green]Correct!")
+        time.sleep(1)
         continue
-    
+
     console.print("[red]Wrong!")
+    time.sleep(1)
+    
 
 os.system("clear")
 
-console.print(f"[blue bold]The quiz is done.\nYou scored {points}/10 points and got {points * 10}% correct!")
+console.print(
+    f"[blue bold]The quiz is done.\nYou scored {points}/10 points and got {points * 10}% correct!"
+)
